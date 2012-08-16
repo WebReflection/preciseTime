061056d76a32d801dd98d94e2a4c32a66870f3ad
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2012-08-16 21:17:57 +0100

    thanks @jdalton for the hint on Chrome

--- build/preciseTime.js
+++ build/preciseTime.js
@@ -11,20 +11,26 @@
     (preciseTime = Performance.now)();
   } catch(o_O) {
     try {
-      // node.js via microtime module + does it work ?
-      (preciseTime = require("microtime").nowDouble)();
+      // Chrome exposes lowercase performance
+      // with a prefixed webkitNow method
+      (preciseTime = performance.webkitNow)();
     } catch(o_O) {
       try {
-        // Rhino + does it work ?
-        var nanoTime = java.lang.System.nanoTime;
-        (preciseTime = function preciseTime() {
-          return nanoTime() / 1000000000;
-        })();
+        // node.js via microtime module + does it work ?
+        (preciseTime = require("microtime").nowDouble)();
       } catch(o_O) {
-        // browsers or fallback ... it works
-        preciseTime = function preciseTime() {
-          return new Date / 1000;
-        };
+        try {
+          // Rhino + does it work ?
+          var nanoTime = java.lang.System.nanoTime;
+          (preciseTime = function preciseTime() {
+            return nanoTime() / 1000000000;
+          })();
+        } catch(o_O) {
+          // browsers or fallback ... it works
+          preciseTime = function preciseTime() {
+            return new Date / 1000;
+          };
+        }
       }
     }
   }
--- test/test.js
+++ test/test.js
@@ -4,34 +4,40 @@
  *  https://github.com/WebReflection/preciseTime
  * --------------------------------------------- */
 
-// JSC has it right, kudos!
-(typeof preciseTime == "undefined" && function(exports){
-  var preciseTime;
-  try {
-    // W3C + does it work ?
-    // Aurora exposes it right now but it does not work in any case
-    (preciseTime = Performance.now)();
-  } catch(o_O) {
-    try {
-      // node.js via microtime module + does it work ?
-      (preciseTime = require("microtime").nowDouble)();
-    } catch(o_O) {
-      try {
-        // Rhino + does it work ?
-        var nanoTime = java.lang.System.nanoTime;
-        (preciseTime = function preciseTime() {
-          return nanoTime() / 1000000000;
-        })();
-      } catch(o_O) {
-        // browsers or fallback ... it works
-        preciseTime = function preciseTime() {
-          return new Date / 1000;
-        };
-      }
-    }
-  }
-  exports.preciseTime = preciseTime;
-}(this));
+ // JSC has it right, kudos!
+ (typeof preciseTime == "undefined" && function(exports){
+   var preciseTime;
+   try {
+     // W3C + does it work ?
+     // Aurora exposes it right now but it does not work in any case
+     (preciseTime = Performance.now)();
+   } catch(o_O) {
+     try {
+       // Chrome exposes lowercase performance
+       // with a prefixed webkitNow method
+       (preciseTime = performance.webkitNow)();
+     } catch(o_O) {
+       try {
+         // node.js via microtime module + does it work ?
+         (preciseTime = require("microtime").nowDouble)();
+       } catch(o_O) {
+         try {
+           // Rhino + does it work ?
+           var nanoTime = java.lang.System.nanoTime;
+           (preciseTime = function preciseTime() {
+             return nanoTime() / 1000000000;
+           })();
+         } catch(o_O) {
+           // browsers or fallback ... it works
+           preciseTime = function preciseTime() {
+             return new Date / 1000;
+           };
+         }
+       }
+     }
+   }
+   exports.preciseTime = preciseTime;
+ }(this));
 
 // since this supposes to work with too many envs
 
