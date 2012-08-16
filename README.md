e1b6e5c3120a74923bb2310b0c3e32a92e2a3a0b
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2012-08-16 22:44:31 +0100

    added process.hrtime() for node, thanks @jdalton again

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ This precision has been [proposed in W3C](http://dvcs.w3.org/hg/webperf/raw-file
 
 Since JSC offers this natively, the aim of this little script is to normalize this function that specially when it comes to benchmarks or asynchronous queue ordered by execution time, could be all we need.
 
-The *node.js* version might require [microtime](https://github.com/wadey/node-microtime).
+The *node.js* version might require [microtime](https://github.com/wadey/node-microtime) if `process.hrtime()` is not supported.
 I have not set the dependency since that module has bindings and it might be more problematic to install.
 
 Browsers that do not implement *HighResolutionTime* will simply return `new Date / 1000` to be consistent with other results.
--- build/preciseTime.js
+++ build/preciseTime.js
@@ -1,35 +1,44 @@
 /*! (C) WebReflection - Mit Style License
+ *  thanks @jdalton for different hints
  *  https://github.com/WebReflection/preciseTime
  * --------------------------------------------- */
 
 // JSC has it right, kudos!
 (typeof preciseTime == "undefined" && function(exports){
-  var preciseTime;
+  var preciseTime, nanoTime;
   try {
     // W3C + does it work ?
-    // Aurora exposes it right now but it does not work in any case
+    // Aurora exposes it right now but it does not work for me
     (preciseTime = Performance.now)();
-  } catch(o_O) {
-    try {
-      // Chrome exposes lowercase performance
-      // with a prefixed webkitNow method
-      (preciseTime = performance.webkitNow)();
-    } catch(o_O) {
-      try {
-        // node.js via microtime module + does it work ?
-        (preciseTime = require("microtime").nowDouble)();
-      } catch(o_O) {
-        try {
-          // Rhino + does it work ?
-          var nanoTime = java.lang.System.nanoTime;
-          (preciseTime = function preciseTime() {
-            return nanoTime() / 1000000000;
-          })();
-        } catch(o_O) {
-          // browsers or fallback ... it works
-          preciseTime = function preciseTime() {
-            return new Date / 1000;
+  } catch(o_O) { try {
+      // apparently few browsers support this
+      (preciseTime = performance.now)();
+    } catch(o_O) { try {
+        // Chrome exposes lowercase performance
+        // with a prefixed webkitNow method
+        (preciseTime = performance.webkitNow)();
+      } catch(o_O) { try {
+          // node.js process.hrtime + does it work ?
+          nanoTime = function () {
+            return parseFloat(process.hrtime().join("."));
           };
+          (preciseTime = nanoTime)();
+        } catch(o_O) { try {
+            // node.js via microtime module + does it work ?
+            (preciseTime = require("microtime").nowDouble)();
+          } catch(o_O) { try {
+              // Rhino + does it work ?
+              nanoTime = java.lang.System.nanoTime;
+              (preciseTime = function preciseTime() {
+                return nanoTime() / 1000000000;
+              })();
+            } catch(o_O) {
+              // browsers or fallback ... it works
+              preciseTime = function preciseTime() {
+                return new Date / 1000;
+              };
+            }
+          }
         }
       }
     }
