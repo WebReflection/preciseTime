b64d9b1bd6ba9fb76146e9da7f9c33b3aa6b95ba
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2012-08-16 20:27:27 +0100

    preciseTime version 0.0.1

--- test/test.js
+++ test/test.js
@@ -1,5 +1,38 @@
 // copy and paste build/preciseTime here
 
+/*! (C) WebReflection - Mit Style License
+ *  https://github.com/WebReflection/preciseTime
+ * --------------------------------------------- */
+
+// JSC has it right, kudos!
+(typeof preciseTime == "undefined" && function(exports){
+  var preciseTime;
+  try {
+    // W3C + does it work ?
+    // Aurora exposes it right now but it does not work in any case
+    (preciseTime = Performance.now)();
+  } catch(o_O) {
+    try {
+      // node.js via microtime module + does it work ?
+      (preciseTime = require("microtime").nowDouble)();
+    } catch(o_O) {
+      try {
+        // Rhino + does it work ?
+        var nanoTime = java.lang.System.nanoTime;
+        (preciseTime = function preciseTime() {
+          return nanoTime() / 1000000000;
+        })();
+      } catch(o_O) {
+        // browsers or fallback ... it works
+        preciseTime = function preciseTime() {
+          return new Date / 1000;
+        };
+      }
+    }
+  }
+  exports.preciseTime = preciseTime;
+}(this));
+
 // since this supposes to work with too many envs
 
 var t = [+new Date / 1000, this.preciseTime()];
