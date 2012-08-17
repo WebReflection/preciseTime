1c1a5f44c107fc1526f86c509cae7d5cc307bd10
Andrea Giammarchi <andrea.giammarchi@gmail.com>
2012-08-17 07:07:20 +0100

    fixed problem with unbound performance object in Chrome + cleanup

--- build/preciseTime.js
+++ build/preciseTime.js
@@ -4,44 +4,42 @@
  * --------------------------------------------- */
 
 // JSC has it right, kudos!
-(typeof preciseTime == "undefined" && function(exports){
-  var preciseTime, nanoTime;
-  try {
-    // W3C + does it work ?
-    // Aurora exposes it right now but it does not work for me
-    (preciseTime = Performance.now)();
-  } catch(o_O) { try {
-      // apparently few browsers support this
-      (preciseTime = performance.now)();
-    } catch(o_O) { try {
-        // Chrome exposes lowercase performance
-        // with a prefixed webkitNow method
-        (preciseTime = performance.webkitNow)();
-      } catch(o_O) { try {
-          // node.js process.hrtime + does it work ?
-          nanoTime = function () {
-            return parseFloat(process.hrtime().join("."));
-          };
-          (preciseTime = nanoTime)();
-        } catch(o_O) { try {
-            // node.js via microtime module + does it work ?
-            (preciseTime = require("microtime").nowDouble)();
-          } catch(o_O) { try {
-              // Rhino + does it work ?
-              nanoTime = java.lang.System.nanoTime;
-              (preciseTime = function preciseTime() {
-                return nanoTime() / 1000000000;
-              })();
-            } catch(o_O) {
-              // browsers or fallback ... it works
-              preciseTime = function preciseTime() {
-                return new Date / 1000;
-              };
-            }
-          }
-        }
-      }
-    }
+!function(exports){
+  var preciseTime = exports.preciseTime;
+  if (!preciseTime) {
+    // all possibilities + inline "does it work?" check
+    try {
+      // W3C Performance instance
+      (preciseTime = function preciseTime() {
+        return performance.now();
+      })();
+    } catch(o_O) {try {
+      // Chrome exposes it via prefixed method
+      (preciseTime = function preciseTime() {
+        return performance.webkitNow();
+      })();
+    } catch(o_O) {try {
+      // node.js v0.8+ process.hrtime
+      (preciseTime = function preciseTime() {
+        return parseFloat(process.hrtime().join("."));
+      })();
+    } catch(o_O) {try {
+      // node.js via microtime module
+      (preciseTime = function preciseTime() {
+        return require("microtime").nowDouble();
+      })();
+    } catch(o_O) {try {
+      // Rhino
+      (preciseTime = function preciseTime() {
+        return java.lang.System.nanoTime() / 1000000000;
+      })();
+    } catch(o_O) {
+      // browsers or fallback
+      // if this throw, this is not a JS env ...
+      (preciseTime = function preciseTime() {
+        return new Date / 1000;
+      })();
+    }}}}}
+    exports.preciseTime = preciseTime;
   }
-  exports.preciseTime = preciseTime;
-}(this));
+}(this);
